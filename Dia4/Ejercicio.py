c47b2b2ce0a3ee2f3886d9f5acfb2d0e8f91ff76
Gersonchm1 <gersonchaparromartinez@gmail.com>
2025-02-18 13:43:48 -0500

    Segundo

---
+++ Dia4/Ejercicio.py
@@ -0,0 +1,13 @@
+lie=True
+while (lie==True):
+    print("##################")
+    print("////inventory////")
+    print("##################")
+    print("Why you want to do?")
+    print("1./// See product list")
+    print("2./// Exit")
+
+    option= int(input(":"))
+    if option==1:
+        print("HI")
+        
